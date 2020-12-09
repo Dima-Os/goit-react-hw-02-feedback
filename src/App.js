996a8fc3b8a4413d37bd1c0faad2de8e6d159783
Dima Osipov <osipovdima1989@gmail.com>
2020-12-09 16:44:06 +0200

    added keys

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import { Component } from 'react';
-import shortid from 'shortid';
+import { generate } from 'shortid';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.getKey();
+  }
   state = { good: 0, neutral: 0, bad: 0 };
+  keys = {
+    btnKeysArr: [],
+    descriptionKeysArr: [],
+  };
+  onLeaveFeedback = e => {
+    const feedbackType = e.currentTarget.innerText;
+    this.setState(prevState => ({
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  getKey = () => {
+    for (let i = 1; i <= Object.keys(this.state).length; i += 1) {
+      this.keys.btnKeysArr.push(generate());
+      this.keys.descriptionKeysArr.push(generate());
+    }
+    console.log(this.keys.btnKeysArr, this.keys.descriptionKeysArr);
+  };
 
   render() {
     return (
       <>
         <h2>Please leave feedback</h2>
-        {Object.keys(this.state).map(el => {
+        {Object.keys(this.state).map((el, idx) => {
           return (
             <button
-              key={shortid.generate()}
+              key={this.keys.btnKeysArr[idx]}
               type="button"
               onClick={this.onLeaveFeedback}
             >
@@ -20,9 +42,9 @@ export default class App extends Component {
           );
         })}
         <h2>Statistics</h2>
-        {Object.keys(this.state).map(el => {
+        {Object.keys(this.state).map((el, idx) => {
           return (
-            <p key={shortid.generate()}>
+            <p key={this.keys.descriptionKeysArr[idx]}>
               <span>{el}: </span>
               <span>{this.state[el]}</span>
             </p>
